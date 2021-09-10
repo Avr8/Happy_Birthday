503709316fed5042b5b2d4259ed70c2188680969
Avr8 <42838937+Avr8@users.noreply.github.com>
2021-09-10 14:20:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+
+import pandas
+import smtplib
+import datetime as dt
+import random
+data = pandas.read_csv(r"D:\us-states-game-start\letter_templates\birthdays.csv")
+data_dict = data.to_dict()
+dtobj= dt.datetime.now()
+my_email = "utest3926@gmail.com"
+pass1 = "Test@1234"
+connection = smtplib.SMTP("smtp.gmail.com")
+if dtobj.month == data_dict['month'][0] and dtobj.day == data_dict['day'][0]:
+    with open(r"D:\us-states-game-start\letter_templates\letter_1.txt") as quote_file:
+        wish = quote_file.read()
+        wish = wish.replace("[Name]","Dad")
+    connection.starttls()
+    connection.login(user=my_email,password=pass1)
+    connection.sendmail(from_addr = my_email,to_addrs = "testuser065@yahoo.com",msg = f"Subject: Happy Birthday! \n\n {wish}")
+    connection.close()
+
+
+
+
+
